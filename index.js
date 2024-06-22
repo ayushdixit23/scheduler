97be4089745a19746ec5f67f798766a09f65372c
Ayush Dixit <117524157+ayushdixit23@users.noreply.github.com>
2024-06-22 11:18:46 +0530

    432

--- index.js
+++ index.js
@@ -58,8 +58,8 @@ cron.schedule('0 */12 * * *', async () => {
 	try {
 		const free = await Membership.findById("65671e5204b7d0d07ef0e796");
 		const users = await User.find({ "memberships.membership": { $ne: free._id } });
-		const workspace = await User.findById("65f5539d09dbe77dea51400d");
-		const workspaceprofilepic = workspace.profilepic
+		// const workspace = await User.findById("65f5539d09dbe77dea51400d");
+		// const workspaceprofilepic = workspace.profilepic
 
 		for (let i = 0; i < users.length; i++) {
 			const endingDate = new Date(users[i].memberships.ending);
@@ -80,60 +80,60 @@ cron.schedule('0 */12 * * *', async () => {
 				users[i].isverified = false
 				await users[i].save()
 
-				const timestamp = `${new Date()}`;
+				// const timestamp = `${new Date()}`;
 
-				const senderpic = process.env.URL + users[i].profilepic;
-				const recpic = process.env.URL + workspaceprofilepic;
+				// const senderpic = process.env.URL + users[i].profilepic;
+				// const recpic = process.env.URL + workspaceprofilepic;
 
 
-				function msgid() {
-					return Math.floor(100000 + Math.random() * 900000);
-				}
-				const mesId = msgid();
-				const convs = await Conversation.findOne({
-					members: { $all: [workspace?._id, users[i]._id] },
-				});
+				// function msgid() {
+				// 	return Math.floor(100000 + Math.random() * 900000);
+				// }
+				// const mesId = msgid();
+				// const convs = await Conversation.findOne({
+				// 	members: { $all: [workspace?._id, users[i]._id] },
+				// });
 
-				if (users[i].notificationtoken) {
+				// if (users[i].notificationtoken) {
 
-					const timestamp = `${new Date()}`;
-					const msg = {
-						notification: {
-							title: `Grovyo Workspace`,
-							body: `Your Membership has Expired.`,
-						},
-						data: {
-							screen: "Conversation",
-							sender_fullname: `${users[i]?.fullname}`,
-							sender_id: `${users[i]?._id}`,
-							text: `Your Membership has Expired.`,
-							convId: `${convs?._id}`,
-							createdAt: `${timestamp}`,
-							mesId: `${mesId}`,
-							typ: "message",
-							senderuname: `${users[i]?.username}`,
-							senderverification: `${users[i].isverified}`,
-							senderpic: `${recpic}`,
-							reciever_fullname: `${workspace.fullname}`,
-							reciever_username: `${workspace.username}`,
-							reciever_isverified: `${workspace.isverified}`,
-							reciever_pic: `${senderpic}`,
-							reciever_id: `${workspace._id} `,
-						},
-						token: users[i]?.notificationtoken,
-					};
+				// 	const timestamp = `${new Date()}`;
+				// 	// const msg = {
+				// 	// 	notification: {
+				// 	// 		title: `Grovyo Workspace`,
+				// 	// 		body: `Your Membership has Expired.`,
+				// 	// 	},
+				// 	// 	data: {
+				// 	// 		screen: "Conversation",
+				// 	// 		sender_fullname: `${users[i]?.fullname}`,
+				// 	// 		sender_id: `${users[i]?._id}`,
+				// 	// 		text: `Your Membership has Expired.`,
+				// 	// 		convId: `${convs?._id}`,
+				// 	// 		createdAt: `${timestamp}`,
+				// 	// 		mesId: `${mesId}`,
+				// 	// 		typ: "message",
+				// 	// 		senderuname: `${users[i]?.username}`,
+				// 	// 		senderverification: `${users[i].isverified}`,
+				// 	// 		senderpic: `${recpic}`,
+				// 	// 		reciever_fullname: `${workspace.fullname}`,
+				// 	// 		reciever_username: `${workspace.username}`,
+				// 	// 		reciever_isverified: `${workspace.isverified}`,
+				// 	// 		reciever_pic: `${senderpic}`,
+				// 	// 		reciever_id: `${workspace._id} `,
+				// 	// 	},
+				// 	// 	token: users[i]?.notificationtoken,
+				// 	// };
 
-					await admin
-						?.messaging()
-						?.sendMulticast(msg)
-						.then((response) => {
-							console.log("Successfully sent message");
-						})
-						.catch((error) => {
-							console.log("Error sending message:", error);
-						});
+				// 	// await admin
+				// 	// 	?.messaging()
+				// 	// 	?.sendMulticast(msg)
+				// 	// 	.then((response) => {
+				// 	// 		console.log("Successfully sent message");
+				// 	// 	})
+				// 	// 	.catch((error) => {
+				// 	// 		console.log("Error sending message:", error);
+				// 	// 	});
 
-				}
+				// }
 				console.log(`Membership of ${users[i].fullname} has expired`);
 			} else {
 				console.log(`Membership of ${users[i].fullname} is active`);
